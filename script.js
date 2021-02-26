bbfbde8448f1b0b086711c3170ded21b9c690f85
ipous <pousig@g.crookcountyschools.org>
2021-02-26 14:11:53 -0800

    dynamic JS function

---
+++ script.js
@@ -0,0 +1,32 @@
+'strict use'
+
+console.log('Hello world');
+
+function addition(a, b, c, d){
+    let sum = a + b + c + d;
+    return sum;
+    
+}
+
+console.log(addition(3, 3, 3, 3));
+
+function subtraction(a, b){
+    let sum = a - b;
+    return sum;
+}
+
+console.log(subtraction(50, 10));
+
+function multiplication(a, b, c){
+    let sum = a * b * c;
+    return sum;
+}
+
+console.log(multiplication(23, 23, 23));
+
+function divide(a, b){
+    let sum = a / b;
+    return sum;
+}
+
+console.log(divide(30, 2));
